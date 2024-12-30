70336a19d779c9d7f5ba171f2ed8e5d4e8b41cca
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2024-12-30 21:55:57 +0330

    feat: add create user and change some fields

--- users/models.py
+++ users/models.py
@@ -1,64 +1,48 @@
-# users/models.py
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.postgres.fields import ArrayField
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.utils import timezone
 from phonenumber_field.modelfields import PhoneNumberField
 
+import learning.models
+
 
 class UserManager(BaseUserManager):
-    def _validate_user_data(self, email, username):
+    def create_user(self, email, firstname, lastname, phone_number, password=None):
         if not email:
-            raise ValueError('The Email field must be set')
-
-        if not username:
-            raise ValueError('The Username field must be set')
-
-        email = self.normalize_email(email)
-        if self.model.objects.filter(email__iexact=email).exists():
-            raise ValueError('User with this email already exists')
-
-        username = username.lower()
-        if self.model.objects.filter(username__iexact=username).exists():
-            raise ValueError('User with this username already exists')
-
-        return email, username
-
-    def create_user(self, email, username, password=None, **extra_fields):
-        email, username = self._validate_user_data(email, username)
+            raise ValueError('Users must have an email address')
 
         user = self.model(
-            email=email,
-            username=username,
-            **extra_fields
+            email=self.normalize_email(email),
+            firstname=firstname,
+            lastname=lastname,
+            phone_number=phone_number,
         )
 
-        if password:
-            user.set_password(password)
-        else:
-            user.set_unusable_password()
-
+        user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, username, password=None, **extra_fields):
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_active', True)
-        extra_fields.setdefault('is_confirmed', True)
+    def create_superuser(self, email, firstname, lastname, phone_number, password=None):
+        user = self.create_user(
+            email=email,
+            firstname=firstname,
+            lastname=lastname,
+            phone_number=phone_number,
+            password=password,
+        )
+        user.is_staff = True
+        user.is_confirmed = True
+        user.save(using=self._db)
+        return user
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError('Superuser must have is_staff=True.')
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
 
-        return self.create_user(email, username, password, **extra_fields)
-
-
-class User(AbstractBaseUser, PermissionsMixin):
+class User(AbstractBaseUser):
     GENDER_CHOICES = [
         (1, 'female'),
         (2, 'male'),
+        (3, 'None')
     ]
 
     USER_TYPE_CHOICES = [
@@ -78,19 +62,11 @@ class User(AbstractBaseUser, PermissionsMixin):
         }
     )
 
-    username = models.CharField(
-        max_length=150,
-        unique=True,
-        error_messages={
-            'unique': "A user with that username already exists.",
-        }
-    )
-
     firstname = models.CharField(max_length=150)
 
     lastname = models.CharField(max_length=150)
 
-    gender = models.IntegerField(choices=GENDER_CHOICES, default=1)
+    gender = models.IntegerField(choices=GENDER_CHOICES, default=3)
 
     phone_number = PhoneNumberField()
 
@@ -110,7 +86,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'firstname', 'lastname', 'phone_number']
+    REQUIRED_FIELDS = ['firstname', 'lastname', 'phone_number']
 
     class Meta:
         verbose_name = 'User'
@@ -120,18 +96,6 @@ class User(AbstractBaseUser, PermissionsMixin):
 
         indexes = [
             models.Index(fields=['email']),
-            models.Index(fields=['username']),
-        ]
-
-        constraints = [
-            models.UniqueConstraint(
-                fields=['email'],
-                name='unique_email_case_insensitive',
-            ),
-            models.UniqueConstraint(
-                fields=['username'],
-                name='unique_username_case_insensitive',
-            ),
         ]
 
     def __str__(self):
@@ -141,24 +105,23 @@ class User(AbstractBaseUser, PermissionsMixin):
     def full_name(self):
         return f'{self.firstname} {self.lastname}'
 
-    def has_valid_subscription(self):
-        if not self.expire_date:
-            return False
-        return self.expire_date >= timezone.now().date()
+    def has_perm(self, perm, obj=None):
+        return self.is_staff
 
-    def get_role(self):
-        if hasattr(self, 'staff'):
-            return dict(Staff.ROLE_CHOICES)[self.staff.role_type]
-        if hasattr(self, 'author_profile'):
-            return 'author'
-        return 'student'
+    def has_module_perms(self, app_label):
+        return self.is_staff
+
+    def get_username(self):
+        return self.email
 
 
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(help_text="Author's biography", null=True, blank=True)
-    specializations = models.JSONField(default=list, help_text="List of areas of expertise", null=True, blank=True)
-    total_courses = models.PositiveIntegerField(default=0)
+    specializations = models.ManyToManyField(
+        learning.models.Category,
+        help_text="Specializations of author",
+        related_name='author', null=True, blank=True)
 
     def __str__(self):
         return self.user.full_name
@@ -167,16 +130,10 @@ class Author(models.Model):
         verbose_name = 'Author'
         verbose_name_plural = 'Authors'
 
-        ordering = ['-total_courses']
-
-    def get_active_courses(self):
-        """Get author's active courses"""
-        return self.courses.filter(is_active=True)
-
 
 class UserCourse(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    course = models.ForeignKey('learning.Course', on_delete=models.CASCADE)  # Create this later
+    courses = models.ManyToManyField('learning.Course', related_name='user_courses')
     progress = models.IntegerField(
         default=0,
         validators=[MinValueValidator(0), MaxValueValidator(100)]
@@ -187,10 +144,9 @@ class UserCourse(models.Model):
     class Meta:
         verbose_name = 'User course'
         verbose_name_plural = 'User courses'
-        unique_together = ['user', 'course']
 
     def __str__(self):
-        return f"{self.user.full_name} - {self.course}"
+        return f"{self.user.full_name} - {self.courses}"
 
 
 class Streak(models.Model):
@@ -204,7 +160,7 @@ class Streak(models.Model):
     ]
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    last_interaction = models.DateField()
+    last_interaction = models.DateField(null=True, blank=True)
     current_streak = models.IntegerField(default=0)
     type = models.IntegerField(choices=STREAK_TYPE_CHOICES)
     highest_streak = models.IntegerField('highest streak', default=0)
@@ -212,7 +168,6 @@ class Streak(models.Model):
     class Meta:
         verbose_name = 'Streak'
         verbose_name_plural = 'Streaks'
-        unique_together = ['user', 'type']
 
     def update_streak(self, interaction_date=None):
         if interaction_date is None:
@@ -234,15 +189,25 @@ class Streak(models.Model):
 
 class UserResponse(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    question = models.ForeignKey('learning.BaseQuestion', on_delete=models.CASCADE)
-    text_answer = models.JSONField(default=list, null=True, blank=True)
-    choice_answer = models.JSONField(default=list, null=True, blank=True)
+
+    question = models.ManyToManyField(learning.models.BaseQuestion, related_name='question_for_user_response')
+
+    text_answer = ArrayField(
+        models.CharField(max_length=255),
+        null=True,
+        blank=True)
+
+    choice_answers = models.ManyToManyField(
+        learning.models.Choice,
+        related_name='correct_choices_for_user_response',
+        help_text="Correct choice options",
+    )
+
     submitted_at = models.DateTimeField('submitted at', auto_now_add=True)
 
     class Meta:
         verbose_name = 'User response'
         verbose_name_plural = 'User responses'
-        ordering = ['-submitted_at']
 
     def __str__(self):
         return f"{self.user.full_name} - {self.question}"
