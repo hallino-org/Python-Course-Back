0d11762cfe47bbe585f4d9098fadf444b431a78f
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2025-02-13 12:14:00 +0330

    chore: delete load_dotenv for use system environment variable

--- Hallino/settings.py
+++ Hallino/settings.py
@@ -13,15 +13,12 @@ import os
 from pathlib import Path
 
 import dj_database_url
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-load_dotenv()
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
