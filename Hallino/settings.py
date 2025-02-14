ca97875e85657646aa82a862b823ee6b19951550
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2025-02-14 17:21:00 +0330

    fix: remove default pagination settings from REST framework configuration

--- Hallino/settings.py
+++ Hallino/settings.py
@@ -88,8 +88,6 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
         'rest_framework.filters.OrderingFilter',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
 }
 
 WSGI_APPLICATION = 'Hallino.wsgi.application'
