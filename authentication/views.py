2e43aa856de339fddd5cd9dccc52b373ed5fdfe2
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2025-02-16 10:53:00 +0330

    feat: add logout view

--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import logout, login
 from rest_framework import status
 from rest_framework.authtoken.models import Token
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -40,3 +40,13 @@ class LoginView(APIView):
                 'token': token.key
             }, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @staticmethod
+    def post(request):
+        request.user.auth_token.delete()
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
