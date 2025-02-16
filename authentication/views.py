62bae9047f0934ce054a53e534b95d18a0bcb7c0
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2025-02-16 10:34:00 +0330

    feat: implement registration view

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from authentication.serializers import RegisterSerializer
+from users.serializers import UserSerializer
+
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    @staticmethod
+    def post(request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                'user': UserSerializer(user).data,
+                'token': token.key
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
