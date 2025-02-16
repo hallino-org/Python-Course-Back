a7a22ca51085afc368f75df2cd47113408a3871f
S.Abolfazl Eftekhari <s.a.eftekhari.b@gmail.com>
2025-02-16 10:45:00 +0330

    feat: add login view

--- authentication/views.py
+++ authentication/views.py
@@ -1,10 +1,11 @@
+from django.contrib.auth import logout, login
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from authentication.serializers import RegisterSerializer
+from authentication.serializers import RegisterSerializer, LoginSerializer
 from users.serializers import UserSerializer
 
 
@@ -21,4 +22,21 @@ class RegisterView(APIView):
                 'user': UserSerializer(user).data,
                 'token': token.key
             }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    @staticmethod
+    def post(request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data
+            token, created = Token.objects.get_or_create(user=user)
+            login(request, user)
+            return Response({
+                'user': UserSerializer(user).data,
+                'token': token.key
+            }, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
